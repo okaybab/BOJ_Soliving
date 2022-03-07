2a87ce209fcfb1e03031e134272404805e91b8ea
okaybab <oki8370@naver.com>
2022-03-08 01:05:27 +0900

    백준 1008 A/B

--- main.cpp
+++ main.cpp
@@ -2,37 +2,17 @@
 
 using namespace std;
 
-bool IsPrime(int num)
-{
-	for (int n = 2; n < num; n++)
-	{
-		if ((num % n) == 0)
-		{
-			return false;
-		}
-	}
-
-	return true;
-}
-
 int main()
 {
-	int num, count = 0;
-	int* N;
-	cin >> num;
+	int A, B;
 
-	N = new int[num];
+	cin >> A >> B;
 
-	for (int i = 1; i != num; i++)
-	{
-		cin >>N[i];
-		if (N[i] != 1 && IsPrime(N[i]))
-		{
-			count ++;
-		}
-	}
-	cout << count;
 
-	delete[]N;
+	double C = (double)A / B; 
+
+	printf("%0.12g",C);
+
+
 	return 0;
 }
