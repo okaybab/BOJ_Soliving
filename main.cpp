52967656b815c57677b27806f347330f4c84a195
okaybab <oki8370@naver.com>
2022-03-10 03:19:21 +0900

    백준 1929 소수 구하기

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+// n * logn?
+//정해진 수들 중에서 곱한값을 빼기
+bool ArrayNumber[1000001];
 
 int main()
 {
 	int A, B;
-
 	cin >> A >> B;
 
 
-	double C = (double)A / B; 
+	for (int i = 2; i <= sqrt(B); i++ )
+	{
+		if (ArrayNumber[i])
+			continue;
 
-	printf("%0.12g",C);
+		for (int j = 2; i * j <= B; j++)
+		{
+			ArrayNumber[i * j] = true;
+			
+		}
+		
+	}
+	
+	ArrayNumber[1] = true;
 
-
+	for (int i = A; i <= B; i++)
+	{
+		if (!ArrayNumber[i])
+		{
+			printf("%d\n", i);
+			
+		}
+	}
 	return 0;
 }
