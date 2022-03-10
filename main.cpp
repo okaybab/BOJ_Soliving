cff1aa5c13e20a42637e1194c0bd895c4940d931
okaybab <oki8370@naver.com>
2022-03-10 22:31:56 +0900

    백준 4948 베르트랑 공준

--- main.cpp
+++ main.cpp
@@ -3,38 +3,54 @@
 
 using namespace std;
 
-// n * logn?
-//정해진 수들 중에서 곱한값을 빼기
-bool ArrayNumber[1000001];
+/* 4948번 베르트랑 공준
+0을 받을때까지 수를받아 소수 갯수를 구하는 문제
+1929문제와 유사 while문과 출력문만 바꿔줌
+*/
+
+bool ArrayNumber[246914];
 
 int main()
 {
-	int A, B;
-	cin >> A >> B;
-
-
-	for (int i = 2; i <= sqrt(B); i++ )
-	{
-		if (ArrayNumber[i])
-			continue;
-
-		for (int j = 2; i * j <= B; j++)
-		{
-			ArrayNumber[i * j] = true;
-			
-		}
-		
-	}
 	
-	ArrayNumber[1] = true;
-
-	for (int i = A; i <= B; i++)
+	while (true)
 	{
-		if (!ArrayNumber[i])
+		int A, B, count = 0;
+
+		cin >> A;
+
+		if (A == 0)
+			break;
+
+		B = A * 2;
+
+		for (int i = 2; i <= sqrt(B); i++)
 		{
-			printf("%d\n", i);
-			
+			if (ArrayNumber[i])
+				continue;
+
+			for (int j = 2; i * j <= B; j++)
+			{
+				ArrayNumber[i * j] = true;
+
+			}
+
 		}
+
+		ArrayNumber[1] = true;
+
+		for (int i = A + 1; i <= B; i++)
+		{
+			if (!ArrayNumber[i])
+			{
+				count++;
+
+			}
+		}
+
+		cout << count << endl;
+
 	}
+
 	return 0;
 }
