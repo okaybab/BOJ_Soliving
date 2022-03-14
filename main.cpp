549d9701f84d7acb1cfa8240aee26fbc916852a6
okaybab <oki8370@naver.com>
2022-03-14 23:26:41 +0900

    백준 10872 팩토리얼

--- main.cpp
+++ main.cpp
@@ -2,29 +2,20 @@
 
 using namespace std;
 
-int num[21];
-
-int Fibonachi(int n)
-{
-	if (n == 0)
-		return n;
-
-	if (n == 1 || n == 2)
-		return num[n] = 1;
-
-	if (num[n] != 0)
-		return num[n];
-
-	return num[n] = Fibonachi(n - 1) + Fibonachi(n - 2);
-
-}
-
-
 int main()
 {
 	int n;
 	cin >> n;
-	cout << Fibonachi(n) << endl;
+
+	if (n == 0)
+		n = 1;
+
+	for (int i = n - 1; 0 < i; i--)
+	{
+		n *= i;
+	}
+
+	cout << n << endl;
 
 	return 0;
 }
