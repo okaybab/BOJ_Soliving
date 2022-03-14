aa513e797df4206fa12edf9276722ba1ff6da61e
okaybab <oki8370@naver.com>
2022-03-14 22:38:23 +0900

    백준 9020 골드바흐의 추측

--- main.cpp
+++ main.cpp
@@ -1,56 +1,66 @@
 #include <iostream>
 #include <math.h>
+#define MAX 100001
 
 using namespace std;
 
-/* 4948번 베르트랑 공준
-0을 받을때까지 수를받아 소수 갯수를 구하는 문제
-1929문제와 유사 while문과 출력문만 바꿔줌
-*/
-
-bool ArrayNumber[246914];
-
-int main()
+bool ArrayNumber[MAX];
+void LoopPrime()
 {
-	
-	while (true)
+	for (int i = 2; i <= sqrt(MAX); i++)
 	{
-		int A, B, count = 0;
+		if (ArrayNumber[i])
+			continue;
 
-		cin >> A;
-
-		if (A == 0)
-			break;
-
-		B = A * 2;
-
-		for (int i = 2; i <= sqrt(B); i++)
+		for (int j = 2; i * j <= MAX - 1; j++)
 		{
-			if (ArrayNumber[i])
-				continue;
-
-			for (int j = 2; i * j <= B; j++)
-			{
-				ArrayNumber[i * j] = true;
-
-			}
+			ArrayNumber[i * j] = true;
 
 		}
-
-		ArrayNumber[1] = true;
-
-		for (int i = A + 1; i <= B; i++)
-		{
-			if (!ArrayNumber[i])
-			{
-				count++;
-
-			}
-		}
-
-		cout << count << endl;
 
 	}
 
+	ArrayNumber[1] = true;
+
+}
+
+int main()
+{
+	LoopPrime();
+
+	int T; 
+
+	cin >> T;
+
+	for (int Tn = 0; Tn < T; Tn++)
+	{
+		int N, A = 0, B = 0;
+		cin >> N;
+
+		if (sqrt(N) == N / 2 )
+		{
+		
+			A = B = N / 2;
+			printf("%d %d\n", A, B);
+			continue;
+		}
+
+		for (int i = (N / 2) - !((N / 2) % 2); 2 < i; i = i - 2)
+		{
+			A = i;
+			B = N - A;
+			
+		
+			if(!ArrayNumber[A] && !ArrayNumber[B])
+			{
+	
+				printf("%d %d\n", A, B);
+				break;
+			}
+
+		}
+	}
+	
 	return 0;
+
 }
