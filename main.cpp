361959fcb80122e5cb2081879ab6f06133f742ba
okaybab <oki8370@naver.com>
2022-03-17 21:20:20 +0900

    백준 2798 블랙잭

--- main.cpp
+++ main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+vector<int> number;
+
+int NumberArray(int n, int m)
+{
+	int sum = 0;
+
+	for (int top_n = n; top_n > 1; top_n--)
+	{
+		for (int middle_n = top_n - 1; middle_n > 0; middle_n--)
+		{
+			for (int end_n = middle_n - 1; end_n > -1; end_n--)
+			{
+				int result = number[top_n] + number[middle_n] + number[end_n];
+
+				if(sum < result && result <= m)
+					sum = result;
+			}
+
+		}
+
+	}
+	return sum;
+}
+
 
 int main()
 {
-	int n;
-	cin >> n;
 
-	if (n == 0)
-		n = 1;
+	int N, M;
+	cin >> N >> M;
 
-	for (int i = n - 1; 0 < i; i--)
+	for (int i = 0; i < N; i++)
 	{
-		n *= i;
+		int A;
+		cin >> A;
+		number.push_back(A);
 	}
 
-	cout << n << endl;
+	
+	cout << NumberArray(number.size() - 1, M);
+	
 
 	return 0;
 }
