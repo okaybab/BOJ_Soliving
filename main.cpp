51179147d234b15cff62ff22c8247f70f19da892
okaybab <oki8370@naver.com>
2022-03-18 23:19:09 +0900

    백준 2750 수 정렬하기

--- main.cpp
+++ main.cpp
@@ -1,37 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> number;
 
-int NumberArray(int n, int m)
-{
-	int sum = 0;
-
-	for (int top_n = n; top_n > 1; top_n--)
-	{
-		for (int middle_n = top_n - 1; middle_n > 0; middle_n--)
-		{
-			for (int end_n = middle_n - 1; end_n > -1; end_n--)
-			{
-				int result = number[top_n] + number[middle_n] + number[end_n];
-
-				if(sum < result && result <= m)
-					sum = result;
-			}
-
-		}
-
-	}
-	return sum;
-}
-
-
 int main()
 {
-
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
 	for (int i = 0; i < N; i++)
 	{
@@ -40,8 +17,12 @@ int main()
 		number.push_back(A);
 	}
 
-	
-	cout << NumberArray(number.size() - 1, M);
+	sort(number.begin(), number.end());
+
+	for (auto it = number.begin(); it < number.end(); it++)
+	{
+		cout << *it << endl;
+	}
 	
 
 	return 0;
