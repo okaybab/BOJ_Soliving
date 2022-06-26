75b354eddd96777031a9bbc384a50387b08b3dba
okaybab <oki8370@naver.com>
2022-06-26 21:33:30 +0900

    백준 2558 A+B-2

--- main.cpp
+++ main.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	int power, n;
-	cin >> n;
+	int a = 0, b = 0;
+	cin >> a >> b;
 
-	for (int i = 0; i < n; i++)
-	{
-		int num = i;
-		int JisuSum = 0;
-
-		for (int jarisu = log10(i); jarisu >= 0; jarisu--)
-		{
-			JisuSum += floor(num / pow(10, jarisu));
-			num -= floor(num / pow(10, jarisu)) * pow(10, jarisu);
-		}
-
-		if (n == (i + JisuSum))
-		{
-			cout << i;
-			return 0;
-		}
-	
-	}
-	cout << 0;
+	cout << a + b << endl;
 
 	return 0;
 }
